bbbec79ab9f1c24dfa82eede018c5d4b76e2bf8d
sldevand <sldeveloperand@gmail.com>
2019-11-03 20:50:16 +0100

    split dependencies files into two files (framework)

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Common\Setup\Installer;
 use App\Modules\Care\Model\Repository\CareRepository;
 use App\Modules\Pet\Controller\PetController;
 use App\Modules\Pet\Model\Repository\PetCareRepository;
@@ -8,73 +7,9 @@ use App\Modules\Pet\Model\Repository\PetImageRepository;
 use App\Modules\Pet\Model\Repository\PetRepository;
 use App\Modules\User\Model\Repository\UserPetRepository;
 use App\Modules\User\Model\Repository\UserRepository;
-use Framework\Db\Pdo\Query\Builder;
-use Framework\Model\Validator\DefaultValidator;
-use Framework\Modules\Installed\Model\Repository\InstalledRepository;
-use Framework\Resource\PDOFactory;
 use Psr\Container\ContainerInterface;
-use Slim\Views\PhpRenderer;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
-// DIC configuration
-/** @var ContainerInterface $container */
-$container = $app->getContainer();
-
-// view renderer
-$container['renderer'] = function (ContainerInterface $c) {
-    $settings = $c->get('settings')['renderer'];
-
-    return new PhpRenderer($settings['template_path']);
-};
-
-// monolog
-$container['logger'] = function (ContainerInterface $c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-
-    return $logger;
-};
-
-// database
-$container['pdo'] = function (ContainerInterface $c) {
-    $settings = $c->get('settings')['pdo']['prod'];
-
-    return PDOFactory::getSqliteConnexion($settings['db-file']);
-};
-
-$container['consoleOutput'] = function (ContainerInterface $c) {
-    return new ConsoleOutput();
-};
-
-$container['queryBuilder'] = function (ContainerInterface $c) {
-    return new Builder();
-};
-
-//validators
-$container['defaultValidator'] = function (ContainerInterface $c) {
-    return new DefaultValidator();
-};
-
-$container['installedRepository'] = function (ContainerInterface $c) {
-    return new InstalledRepository(
-        $c->get('pdo'),
-        $c->get('defaultValidator')
-    );
-};
-
-// InstallDatabase
-$container['installer'] = function (ContainerInterface $c) {
-    return new Installer(
-        $c->get('pdo'),
-        $c->get('settings')['pdo']['prod']['db-file'],
-        $c->get('consoleOutput'),
-        $c->get('queryBuilder'),
-        $c->get('installedRepository')
-    );
-};
-
+require_once FRAMEWORK_DIR . '/dependencies.php';
 
 // controllers
 $container['petController'] = function (ContainerInterface $c) {

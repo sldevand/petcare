9171c4f941299367695af3d51ab2b3c20b939f7f
sldevand <sldeveloperand@gmail.com>
2020-01-12 20:50:24 +0100

    removed tuupola cors middleware

--- src/middleware.php
+++ src/middleware.php
@@ -12,4 +12,14 @@ $app->add(new JwtAuthentication(
     ]
 ));
 
-$app->add(new Tuupola\Middleware\CorsMiddleware());
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});

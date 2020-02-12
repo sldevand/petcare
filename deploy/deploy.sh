53977175c021eb472f2bd31ff0736672d37b53ce
sldevand <sldeveloperand@gmail.com>
2020-02-12 22:22:37 +0100

    finished deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -24,9 +24,6 @@ GIT_BRANCH=develop
 REMOTE_HOST=pi@raspi3
 REMOTE_WWW_PATH=/home/pi/tmp
 REMOTE_APP_NAME=petcare
-REMOTE_APP_LINK=petcare-api
-REMOTE_APP_PATH=$REMOTE_WWW_PATH/$REMOTE_APP_NAME
-REMOTE_APP_LINK_PATH=$REMOTE_WWW_PATH/$REMOTE_APP_LINK
 REMOTE_SCRIPT_PATH=$LOCAL_APP_PATH/deploy/remote/commands.sh
 
 myEcho "***START $LOCAL_APP_NAME deployer script START***"
@@ -40,6 +37,7 @@ cd $LOCAL_BUILD_PATH
 git clone --single-branch --branch $GIT_BRANCH $GIT_PATH
 
 myEcho "Local : Remove unused files for production"
+rm -rfv $LOCAL_REPO_PATH/deploy
 rm -rfv $LOCAL_REPO_PATH/src/.env*
 rm -rfv $LOCAL_REPO_PATH/tests
 rm -rfv $LOCAL_REPO_PATH/var/db/.gitkeep
@@ -48,11 +46,14 @@ rm -rfv $LOCAL_REPO_PATH/*.md
 rm -rfv $LOCAL_REPO_PATH/*.xml
 rm -rfv $LOCAL_REPO_PATH/.git
 
-#TODO add remote .env files
 myEcho "***END $LOCAL_APP_NAME deployer script END***"
+
+ssh-add ~/.ssh/raspi3_key
+
+ssh $REMOTE_HOST rm -rvf $REMOTE_WWW_PATH/$REMOTE_APP_NAME
 
 scp -r $LOCAL_REPO_PATH $REMOTE_HOST:$REMOTE_WWW_PATH
 
-ssh $REMOTE_HOST 'bash -s' < $REMOTE_SCRIPT_PATH
+rm -rf $LOCAL_BUILD_PATH
 
-#rm -rf $LOCAL_BUILD_PATH
+ssh $REMOTE_HOST 'bash -s' < $REMOTE_SCRIPT_PATH
--- deploy/remote/commands.sh
+++ deploy/remote/commands.sh
@@ -1,1 +1,42 @@
 #!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+function myEcho(){
+    echo ""
+    echo "${green}--> $1 ${reset}"
+}
+
+myEcho "Executing remote script"
+
+REMOTE_HOST=pi@raspi3
+REMOTE_WWW_PATH=/var/www
+REMOTE_APP_NAME=petcare
+REMOTE_APP_LINK=petcare-api
+REMOTE_APP_PATH=$REMOTE_WWW_PATH/$REMOTE_APP_NAME
+REMOTE_APP_LINK_PATH=$REMOTE_WWW_PATH/$REMOTE_APP_LINK
+REMOTE_COMPOSER=/usr/sbin/composer
+REMOTE_ENV_FILE=/home/pi/deployScripts/petcare/.env
+
+myEcho "Composer Install"
+cd $REMOTE_APP_PATH
+sudo $REMOTE_COMPOSER install
+sudo rm -rfv $REMOTE_APP_PATH/composer.*
+
+myEcho "Add src/.env file"
+sudo cp $REMOTE_ENV_FILE $REMOTE_APP_PATH/src
+
+myEcho "Install database"
+sudo chmod +x $REMOTE_APP_PATH/bin/console
+sudo $REMOTE_APP_PATH/bin/console setup:install
+
+myEcho "Creating symbolic link"
+sudo ln -s $REMOTE_APP_PATH/public $REMOTE_WWW_PATH/$REMOTE_APP_LINK
+
+myEcho "Giving correct rights"
+sudo chown -R www-data:www-data $REMOTE_APP_PATH
+sudo chown -R www-data:www-data $REMOTE_WWW_PATH/$REMOTE_APP_LINK
+sudo find $REMOTE_APP_PATH -type d -exec chmod 0755 {} \;
+sudo find $REMOTE_APP_PATH -type f -exec chmod 0644 {} \;
